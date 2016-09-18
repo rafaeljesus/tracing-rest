bbd435a28fd0c774fba5e57e817c841e26199710
Rafael Jesus <rafaelljesus86@gmail.com>
2016-09-18 13:38:39 -0300

    Change ci

--- db/gorm.go
+++ db/gorm.go
@@ -4,17 +4,12 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/rafaeljesus/tracing-rest/support"
-	"os"
 )
 
 var Repo *gorm.DB
 
 func Connect() error {
-	if os.Getenv("TRACKING_REST_DB") == "" {
-		os.Setenv("TRACKING_REST_DB", support.Config["TRACKING_REST_DB"])
-	}
-
-	conn, err := gorm.Open("postgres", os.Getenv("TRACKING_REST_DB"))
+	conn, err := gorm.Open("postgres", support.Config["TRACKING_REST_DB"])
 	if err != nil {
 		panic(err)
 	}
--- db/migrations/20160904120836_CreateDatabase.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-create database tracing_rest_dev;
-create database tracing_rest_test;
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-drop database database tracing_rest_dev;
-drop database tracing_rest_test;
