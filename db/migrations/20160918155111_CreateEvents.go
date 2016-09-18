6427d979b40a668ca3b785977a72122c0942538b
Rafael Jesus <rafaelljesus86@gmail.com>
2016-09-18 15:56:16 -0300

    Change ci

---
+++ db/migrations/20160918155111_CreateEvents.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+)
+
+func Up_20160918155111(txn *sql.Tx) {
+	_, err := txn.Exec(
+		"create table events (" +
+			"id serial primary key," +
+			"name text not null," +
+			"status text," +
+			"payload jsonb," +
+			"created_at timestamptz not null," +
+			"updated_at timestamptz not null," +
+			"deleted_at timestamptz);")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Down_20160918155111(txn *sql.Tx) {
+	_, err := txn.Exec("drop table events;")
+	if err != nil {
+		panic(err)
+	}
+}
